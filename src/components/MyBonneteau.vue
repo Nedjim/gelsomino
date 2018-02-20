ed1866837abd972023e618c6875b507bab4f63b6
Steven Yung <stevenyung@icloud.com>
2018-02-20 14:45:31 +0100

    setup Shell.vue w/ GET call to random generator URL

--- src/components/MyBonneteau.vue
+++ src/components/MyBonneteau.vue
@@ -1,12 +1,48 @@
 <template>
-  <div>test</div>
+  <div>
+    <div v-for="shell in shells" :key="shell.id" id="shells">
+      <shell :id="shell.id"></shell>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Shell from './Shell.vue';
+
 export default {
   name: 'MyBonneteau',
+  mounted() {
+    axios.get('https://www.random.org/integers/?num=1&min=0&max=2&col=1&base=10&format=plain&rnd=new')
+      .then((response) => {
+        this.perlPosition = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  data() {
+    return {
+      perlPosition: 0,
+      shells: [
+        {
+          id: 0
+        },
+        {
+          id: 1
+        },
+        {
+          id: 2
+        },
+      ]
+    };
+  },
+  components: { Shell }
 }
 </script>
 
 <style scoped>
+#shells {
+  width: 100%;
+}
 </style>
---
+++ src/components/Shell.vue
@@ -0,0 +1,21 @@
+<template>
+  <img src="/static/shell.png" alt="">
+</template>
+
+<script>
+export default {
+  name: 'Shell',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 33%
+}
+</style>
